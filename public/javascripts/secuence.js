23319cc41be43186a24c200b0c9518a5eef4da3b
Jazz <99265919+Desarrollo5HQ5@users.noreply.github.com>
2023-03-10 09:05:20 -0500

    searching err con prueba sonido

--- public/javascripts/secuence.js
+++ public/javascripts/secuence.js
@@ -108,7 +108,7 @@ videoButton.onclick=()=>{
     switch(videoButton.textContent){
         case 'Probar sonido':
             texto.textContent='Por favor, lea toda instrucción antes de continuar.\nPrimero de click al botón <<Iniciar prueba>>. \nLuego lea SOLO la oración que se encuentra entre comillas en voz alta. \n Después de click en el botón <<Terminar>> que aparecerá. \nLa oración que debe decir es: "Esto es una prueba"';
-            videoButton.textContent ='Listo'; 
+            videoButton.textContent ='Iniciar prueba'; 
             break;
         case 'Iniciar prueba':  
 
@@ -297,6 +297,7 @@ try {
   }
   catch(e) {
     //console.error(e);
+    testConsole.textContent=e
     $('.no-browser-support').show();
     $('.app').hide();
   }
